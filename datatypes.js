d72ef4f22485589a8115e68ecca07a1bb550900a
bhaviljain <bhaviljain26@gmail.com>
2023-09-30 12:23:48 +0530

    DATATYPES

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//alert(3+3) we are using node , not browser
+
+console.log(3+3)
+
+let name ="hitesh" //string
+let age = 18 //number , big numbers hain toh: bigint 
+let isloggedin = true //bool
+// undefined
+//null khali haai, standalone value
+//symbol : unique
+
+//object
+
+console.log(typeof "bhavil");
